d3fa49d925d89ee7cede42b706edc97569c46b20
season91 <choayoung91@naver.com>
2021-03-24 17:36:24 +0900

    mypage mvc 생성

---
+++ aboo/src/main/java/com/kh/aboo/mypage/model/repository/MypageRepository.java
@@ -0,0 +1,8 @@
+package com.kh.aboo.mypage.model.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MypageRepository {
+
+}
---
+++ aboo/src/main/java/com/kh/aboo/mypage/model/service/MypageService.java
@@ -0,0 +1,5 @@
+package com.kh.aboo.mypage.model.service;
+
+public interface MypageService {
+
+}
