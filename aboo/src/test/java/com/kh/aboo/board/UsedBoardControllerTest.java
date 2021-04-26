9ed5231b5d82794e553aa9e306acf1ca8615487b
suny1031 <psuny1031@naver.com>
2021-04-26 21:29:51 +0900

    4월 26일 오후 10시
    중고게시판 테스트 구현
    수정 반영

--- aboo/src/test/java/com/kh/aboo/board/UsedBoardControllerTest.java
+++ aboo/src/test/java/com/kh/aboo/board/UsedBoardControllerTest.java
@@ -1,6 +1,7 @@
 package com.kh.aboo.board;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.kh.aboo.user.generation.model.vo.Generation;
+
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class) 
 @ContextConfiguration(locations = {"file:aboo/src/main/webapp/WEB-INF/spring/**/*-context.xml"}) 
@@ -34,6 +37,108 @@ public class UsedBoardControllerTest {
       this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
     
+    @Test
+    public void usedPrivate_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/usedprivate")
+    			.sessionAttr("generation", generation)
+    			.param("usedIdx","u100276")
+    			).andDo(print());
+    	
+    }
+    
+    @Test
+    public void usedDelete_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/useddelete")
+    			.sessionAttr("generation", generation)
+    			.param("usedIdx","u100276")
+    			).andDo(print());
+    	
+    }
+    
+    @Test
+    public void usedCmtUpload_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/usedcmtupload")
+    			.sessionAttr("generation", generation)
+    			.param("usedIdx","u100276")
+    			.param("usedCmtContent","댓글테스트")
+    			).andDo(print());
+    	
+    }
+    
+    @Test
+    public void usedBrdCmtModify_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/usedbrdcmtmodify")
+    			.sessionAttr("generation", generation)
+    			.param("usedCmtIdx","100260")
+    			.param("usedCmtContent","댓글테스트2")
+    			).andDo(print());
+    	
+    }
    
-	   
+    @Test
+    public void usedBrdCmtDelete_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/usedbrdcmtdelete")
+    			.sessionAttr("generation", generation)
+    			.param("usedCmtIdx","100260")
+    			).andDo(print());
+    	
+    }   
+    
+    @Test
+    public void usedBrdCmtPrivate_success() throws Exception {
+    	Generation generation = new Generation();
+    	generation.setId("grf000d000h");
+    	generation.setPassword("park1003*");
+    	generation.setBuilding("000");
+    	generation.setNum("000");
+    	generation.setGenerationIdx("100522");
+    	generation.setApartmentIdx("100000");
+
+    	this.mockMvc.perform(post("/board/used/usedbrdcmtprivate")
+    			.sessionAttr("generation", generation)
+    			.param("usedCmtIdx","100260")
+    			).andDo(print());
+    	
+    } 
+    
 }
