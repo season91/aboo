1fd3a8ba8fabb38970646f9d9066cf2d3e3a9469
season91 <choayoung91@naver.com>
2021-06-17 22:19:48 +0900

    test 수정

---
+++ aboo/src/test/java/com/kh/aboo/bdmin/ManagementControllerTest.java
@@ -0,0 +1,39 @@
+package com.kh.aboo.bdmin;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.kh.aboo.bdmin.management.model.repository.ManagementRepository;
+
+@WebAppConfiguration 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public class ManagementControllerTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ManagementRepository managementRepository;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+		
+	}
+	
+	
+}
